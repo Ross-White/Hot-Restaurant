85e17d590f3bad46891572eb2614ad7813980c8d
wael ahmed <w.ahmed020@gmail.com>
2021-05-01 14:05:31 +0100

    code updates

--- server.js
+++ server.js
@@ -31,5 +31,19 @@ server.get('/api/table', (req, res) => res.json(tables));
 server.get('/api/waitlist', (req, res) => res.json(waitlist));
 
 
+server.post('/api/table', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newBooking = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // newBooking.routeName = newBooking.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newBooking);
+  
+    tables.push(newBooking);
+    res.json(newBooking);
+  });
+
 
 server.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
