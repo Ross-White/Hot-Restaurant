0eaaa6dbd42503a51f4ec1ce1cdd8ed0da15adb7
Ross White <rosswhite@outlook.com>
2021-05-01 13:02:01 +0100

    New routes

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 // const http = require('http');
 
 const server = express();
 const PORT = 3000;
 
+// Routes
 server.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+server.get('/table', (req, res) => res.sendFile(path.join(__dirname, 'table.html')));
+
+server.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 
 // const server = http.createServer((req, res) => {
 //     fs.readFile(`${__dirname}/index.html`, (err, data) => {
